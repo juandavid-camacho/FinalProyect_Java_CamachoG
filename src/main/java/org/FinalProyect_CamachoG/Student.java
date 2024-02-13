5f849fe196d0e73af2f44e08624c9689e9009341
juandavid-camacho <159383714+juandavid-camacho@users.noreply.github.com>
2024-02-13 17:47:47 -0500

    Delete src/main/java/org/FinalProyect_CamachoG directory
    An effort to organize correctly

--- src/main/java/org/FinalProyect_CamachoG/Student.java
+++
@@ -1,9 +0,0 @@
-package main.java.org.FinalProyect_CamachoG;
-
-public class Student extends Person {
-
-    public Student(String name, int age, int ID) {
-        super(name, age, "STU" + ID);
-    }
-
-}
