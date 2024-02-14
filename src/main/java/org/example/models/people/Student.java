1af57e4361a701e563300192e056ea066940b758
juandavid-camacho <159383714+juandavid-camacho@users.noreply.github.com>
2024-02-13 19:57:36 -0500

    Delete src/main/java/org directory

--- src/main/java/org/example/models/people/Student.java
+++
@@ -1,9 +0,0 @@
-package main.java.org.example.models.people;
-
-public class Student extends Person {
-
-    public Student(String name, int age, int ID) {
-        super(name, age, "STU" + ID);
-    }
-
-}
